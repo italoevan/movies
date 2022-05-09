1aedf2e4e25fab1fb8270933bf1a6ca7e95dede3
Italo Gabriel <igbehh@gmail.com>
2022-05-09 09:46:33 -0300

    feat: lock screen orientation

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:movies/src/core/theme/app_theme.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  @override
+  void initState() {
+    _lockScreenOrientation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,5 +24,13 @@ class AppWidget extends StatelessWidget {
       routeInformationParser: Modular.routeInformationParser,
       theme: AppTheme.appTheme,
     );
+  }
+
+  void _lockScreenOrientation() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft
+    ]);
   }
 }
